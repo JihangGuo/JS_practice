3b035f96ef82cce972405d1768361c4e12c1d5db
JihangGuo <JihangGuo@gmail.com>
2017-03-11 17:21:07 +0800

    js学习初期代码

---
+++ JS练习/js/1.1.js
@@ -0,0 +1,27 @@
+/**
+ * Created by hang on 16-11-29.
+ */
+
+function changHeight(){
+    var Height=document.getElementById("one");
+    Height.style.height = 200 +"px";
+}
+function changeWidth(){
+    var Width=document.getElementById("one");
+    Width.style.width = 200 +"px";
+}
+function changColor(){
+    var Color=document.getElementById("one");
+    Color.style.backgroundColor = "red" ;
+}
+function changHidden(){
+    var Hidden=document.getElementById("one");
+    Hidden.style.visibility = "hidden" ;
+}
+function changSet(){
+    var Changeset=document.getElementById("one");
+    Changeset.style.height = 100 +"px";
+    Changeset.style.width = 100 +"px";
+    Changeset.style.backgroundColor = "black";
+    Changeset.style.visibility = "visible" ;
+}
---
+++ JS练习/js/1.2.js
@@ -0,0 +1,11 @@
+/**
+ * Created by hang on 16-11-29.
+ */
+function changeBackC() {
+    var backC=document.getElementById("one");
+    backC.style.backgroundColor = "yellow";
+}
+function changeBackI() {
+    var backI=document.getElementById("one");
+    backI.style.backgroundImage = "../css/signbg.jpg" ;
+}
